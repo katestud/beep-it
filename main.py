c9a0564935212bca09f43f59f1d2d1fc4cab1db4
Kate Studwell <katestudwell@gmail.com>
2025-03-07 12:31:53 -0500

    Wire up a touch sensor

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from machine import Pin
 import time
 
-touch_sensor = Pin(15, Pin.IN)  # GP15 as input
+# Make sure this is attached to 3.3V instead of 5V to avoid damaging the board
+TOUCH_SENSOR = Pin(15, Pin.IN)
 
 while True:
-    if touch_sensor.value():
+    if TOUCH_SENSOR.value():
         print("Touch detected!")
     else:
         print("No touch detected!")
